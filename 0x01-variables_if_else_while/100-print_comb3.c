da569cb7b0c60e9ff55c899dd8f085837f5f8c7b
DuncanNM <duncannmurigu@gmail.com>
2022-03-15 01:30:21 +0300

    Fixed errors in non compliance of betty coding style in two files

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,7 +18,7 @@ int main(void)
 			putchar(a);
 			putchar(b);
 
-			if ((a != '9') || (a == '9' && b != '9'))
+			if ((a != '8') || (a == '8' && b != '9'))
 			{
 				putchar(',');
 				putchar(' ');
@@ -30,4 +30,3 @@ int main(void)
 
 	return (0);
 }
-
