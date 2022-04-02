6525a33a4beb7529f14c09f52c3e0c886ffcf659
DuncanNM <duncannmurigu@gmail.com>
2022-04-02 01:17:18 -0700

    Attempting to fix error 10

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Prints a string of text.
- *
- * Return: Always 0 (Success)
+ * _puts - prints a string to stdout
+ *  @str: the string to be printed
  */
-int main(void)
+void _puts(char *s)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	while (*s)
+		_putchar(*s++);
+
+	_putchar('\n');
 }
